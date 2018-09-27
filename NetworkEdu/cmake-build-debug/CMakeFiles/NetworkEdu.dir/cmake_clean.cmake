78b09e4871da58020a1c75966b71ed28c44e1639
Hitoshi Hoshino <hhoshino-main@memoad.jp>
2018-09-27 17:06:37 +0900

    First Code Commit.

---
+++ NetworkEdu/cmake-build-debug/CMakeFiles/NetworkEdu.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/NetworkEdu.dir/main.c.o"
+  "NetworkEdu.pdb"
+  "NetworkEdu"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/NetworkEdu.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
