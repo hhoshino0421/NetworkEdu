eb7d8242704f00a0cc14b4a8968f9481449ffc87
Hitoshi Hoshino <hhoshino-main@memoad.jp>
2018-10-23 19:25:21 +0900

    Temporary commit 2018/10/23.

--- NetworkEdu/udp.c
+++ NetworkEdu/udp.c
@@ -14,5 +14,35 @@
 #include <arpa/inet.h>
 #include <pthread.h>
 #include "sock.h"
+#include "ether.h"
+#include "ip.h"
+#include "icmp.h"
+#include "dhcp.h"
+#include "udp.h"
+#include "param.h"
+
+extern PARAM    Param;
+
+struct pseudo_ip {
+    struct in_addr  ip_src;
+    struct in_addr  ip_dst;
+    u_int8_t        dummy;
+    u_int8_t        ip_p;
+    u_int16_t       ip_len;
+};
+
+#define     UDP_TABLE_NO    (16)
+
+typedef struct {
+    u_int16_t port;
+} UDP_TABLE;
+
+UDP_TABLE       UpdTable[UDP_TABLE_NO];
+
+pthread_rwlock_t    UpdTableLock = PTHREAD_RWLOCK_INITIALIZER;
 
 
+int print_udp(struct udphr *udp) {
+    printf("udp-------------------------------------------------------\n");
+    
+}
