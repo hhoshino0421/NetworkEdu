8c377350820bd029092d1b211aef595deaa283d6
Hitoshi Hoshino <hhoshino-main@memoad.jp>
2018-11-04 09:37:13 +0900

    Temporary commit 2018/11/04.

--- NetworkEdu/udp.c
+++ NetworkEdu/udp.c
@@ -141,3 +141,178 @@ int UdpSearchTable(u_int16_t port) {
     return -1;
 
 }
+
+
+int UdpShowTable() {
+
+    int     i;
+
+    pthread_rwlock_rdlock(&UdpTableLock);
+
+    for(i = 0; i < UDP_TABLE_NO; i++) {
+
+        if (UdpTable[i].port != 0) {
+            printf("UDP:%d:%u\n", i, UdpTable[i].port);
+        }
+    }
+
+    pthread_rwlock_unlock(&UdpTableLock);
+
+    return 0;
+}
+
+
+u_int16_t UdpSearchFreePort() {
+
+    u_int16_t   i;
+
+    for( i = 32768;i < 61000; i++) {
+
+        if(UdpSearchTable(i) == -1) {
+            return -1;
+        }
+
+    }
+
+    return 0;
+}
+
+
+int UdpSocket(u_int16_t port) {
+
+    int no;
+
+    if (port == DHCP_CLIENT_PORT) {
+        printf("UdpSocket:port %d can not use\n", port);
+        return -1;
+    }
+
+    if (port == 0) {
+
+        if ((port = UdpSearchFreePort()) == 0) {
+            printf("UdpSocket: no free port\n");
+            return -1;
+        }
+
+    }
+
+    no = UdpAddTable(port);
+
+    if (no == -1) {
+        return -1;
+    }
+
+    return 0;
+}
+
+
+int UdpSocketClose(u_int16_t port) {
+
+    int     no;
+
+    no = UdpSearchTable(port);
+
+    if (no == -1 ) {
+        printf("UdpSocketClose:%u:not exists\n",port);
+        return -1;
+    }
+
+    pthread_rwlock_wrlock(&UdpTableLock);
+
+    UdpTable[no].port = 0;
+    pthread_rwlock_unlock(&UdpTableLock);
+
+    return 0;
+
+}
+
+
+int UdpSendLink(int soc, u_int8_t smac[6], u_int8_t dmac[6], struct in_addr *saddr, struct in_addr *daddr
+        ,u_int16_t sport, u_int16_t dport, int dontFlagment, u_int8_t *data, int len) {
+
+
+    u_int8_t        *ptr, sbuf[64 * 1024];
+    struct  udphdr  *udp;
+
+    ptr = sbuf;
+    udp = (struct udphdr *)ptr;
+    memset(udp, 0, sizeof(struct udphdr));
+    udp->source = htons(sport);
+    udp->dest = htons(dport);
+    udp->len = htons(sizeof(struct udphdr) + len);
+    udp->check = 0;
+    ptr += sizeof(struct udphdr);
+
+    memcpy(ptr, data, len);
+    ptr += len;
+    udp->check = UdpChecksum(saddr, daddr, IPPROTO_UDP, sbuf, ptr - sbuf);
+
+    printf("==== UDP ===[\n");
+
+    IpSendLink(soc, smac, dmac, saddr, daddr, IPPROTO_UDP, dontFlagment, Param.IpTTL, ptr - sbuf);
+    print_udp(udp);
+    print_hex(data, len);
+    printf("]\n");
+
+    return 0;
+
+}
+
+
+int UdopSend(int soc, struct in_addr *saddr, struct in_addr *daddr,u_int16_t sport,u_int16_t dport
+        , int dontFlagment,u_int8_t *data,int len) {
+
+    u_int8_t            *ptr,   sbuf[64*1024];
+    struct  udphdr      *udp;
+
+
+    ptr=sbuf;
+    udp = (struct udhhdr *)ptr;
+    memset(udp, 0, sizeof(struct udphdr));
+    udp->source = htons(sport);
+    udp->dest = htons(dport);
+    udp->len = htons(sizeof(struct udphdr) + len);
+    udp->check = 0;
+    ptr += sizeof(struct udphdr);
+
+    memcpy(ptr, data, len);
+    ptr += len;
+    udp->check = UdpChecksum(saddr, daddr, IPPROTO_UDP, sbuf, ptr - sbuf);
+
+    printf("=== UDP ===[\n");
+    IpSend(soc, saddr, daddr, IPPROTO_UDP, dontFlagment, Param.IpTTL, sbuf, ptr - sbuf);
+    print_udp(udp);
+    print_hex(data, len);
+    printf("]\n");
+
+    return 0;
+
+}
+
+
+int UdpRecv(int soc, struct ether_header *eh, struct ip *ip,u_int8_t *data,int len) {
+
+    struct  udphdr      *udp;
+    u_int8_t            *ptr = data;
+    u_int16_t           sum;
+    int                 udplen;
+
+    udplen = len;
+
+    sum=UdpChecksum(&ip->ip_src,&ip->ip_dst,ip->ip_p,data,udplen);
+
+    if (sum!=0&&sum!=0xFFFF) {
+
+        printf("UdpRecv:bad udp checksum(%x):udplen=%u\n",sum,udplen);
+        return -1;
+
+    }
+
+    udp=(struct udphdr *)ptr;
+    ptr+=sizeof(struct udphdr);
+    udplen-=sizeof(struct udphdr);
+
+    
+
+
+}
